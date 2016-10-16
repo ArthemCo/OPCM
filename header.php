7ab3e8906268f8f17e5f20eb8448e390fa2a01db
Michael Hemingway <mikemingway@gmail.com>
2016-10-15 21:57:44 -0400

    login css, js updates and template structure

--- header.php
+++ header.php
@@ -4,14 +4,9 @@
 
 	<head>
 		<meta charset="utf-8">
-		
-		<!-- Force IE to use the latest rendering engine available -->
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-		<!-- Mobile Meta -->
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta class="foundation-mq">
-		
+
 		<!-- If Site Icon isn't set in customizer -->
 		<?php if ( ! function_exists( 'has_site_icon' ) || ! has_site_icon() ) { ?>
 			<!-- Icons & Favicons -->
@@ -22,8 +17,8 @@
 			<![endif]-->
 			<meta name="msapplication-TileColor" content="#f01d4f">
 			<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/assets/images/win8-tile-icon.png">
-	    	<meta name="theme-color" content="#121212">
-	    <?php } ?>
+	  <?php } ?>
+    <meta name="theme-color" content="#121212">
 
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
@@ -33,23 +28,7 @@
 		<!-- end analytics -->
 
 	</head>
-	
-	<!-- Uncomment this line if using the Off-Canvas Menu --> 
-		
 	<body <?php body_class(); ?>>
-
-		<div class="off-canvas-wrapper">
-			
-			<div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
-				
-				<?php get_template_part( 'parts/content', 'offcanvas' ); ?>
-				
-				<div class="off-canvas-content" data-off-canvas-content>
-					
-					<header class="header" role="banner">
-							
-						 <!-- This navs will be applied to the topbar, above all content 
-							  To see additional nav styles, visit the /parts directory -->
-						 <?php get_template_part( 'parts/nav', 'offcanvas-topbar' ); ?>
-		 	
-					</header> <!-- end .header -->
+		<nav class="header" role="banner">
+			 <?php get_template_part( 'parts/nav' ); ?>
+		</nav> <!-- end .header -->
