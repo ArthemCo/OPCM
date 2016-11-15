f8b188558f1fa9c6182d7885e60b6dbb7302d6c6
Michael Hemingway <mikemingway@gmail.com>
2016-11-15 16:38:31 -0500

    Mobile CSS tweaks and jQ removal

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,28 +3,9 @@
  * Michael Hemingway
  */
 
-(function ($) {
+(function () {
 	'use strict';
-	$(document).ready(function () {
-		//
-		// // Adds Flex Video to YouTube and Vimeo Embeds
-		// $('iframe[src*="youtube.com"], iframe[src*="vimeo.com"]').each(function () {
-		// 	if ($(this).innerWidth() / $(this).innerHeight() > 1.5) {
-		// 		$(this).wrap("<div class='widescreen flex-video'/>");
-		// 	} else {
-		// 		$(this).wrap("<div class='flex-video'/>");
-		// 	}
-		// });
-
-		// window.sr = ScrollReveal();
-		// sr.reveal('p', {
-		// 	scale: 1,
-		// 	duration: 600,
-		// 	distance: '30px'
-		// });
+	document.addEventlistener('load', function () {
 
 	});
-
-
-
-}(jQuery));
+}());
