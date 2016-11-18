6b700fe6ebab8a79e5e2e5fb5c161fe512370e25
Michael Hemingway <mikemingway@gmail.com>
2016-11-18 14:57:26 -0500

    version 1.0 pre-delivery
    added calling card js

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,16 @@
 (function ($) {
 	'use strict';
 	$(function () {
-
+		// Our company signature :P
+		var t = "color: #bada55; background: #222";
+		console.log("%c  █████╗ ██████╗ ████████╗██╗  ██╗███████╗███╗   ███╗     ██████╗ ██████╗    ", t);
+		console.log("%c ██╔══██╗██╔══██╗╚══██╔══╝██║  ██║██╔════╝████╗ ████║    ██╔════╝██╔═══██╗   ", t);
+		console.log("%c ███████║██████╔╝   ██║   ███████║█████╗  ██╔████╔██║    ██║     ██║   ██║   ", t);
+		console.log("%c ██╔══██║██╔══██╗   ██║   ██╔══██║██╔══╝  ██║╚██╔╝██║    ██║     ██║   ██║   ", t);
+		console.log("%c ██║  ██║██║  ██║   ██║   ██║  ██║███████╗██║ ╚═╝ ██║    ╚██████╗╚██████╔╝██╗", t);
+		console.log("%c ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝     ╚═════╝ ╚═════╝ ╚═╝", t);
+		console.log("Site developed with love by Arthem.");
+		console.log("https://arthem.co/ ❤");
 	});
 
 }(jQuery));
