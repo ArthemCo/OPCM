6e6f37788bc5cb1bd4239263e6b60a6e38af3948
Michael Hemingway <mikemingway@gmail.com>
2016-11-21 23:45:33 -0500

    multilingual support, I think

--- page-home.php
+++ page-home.php
@@ -11,15 +11,15 @@ Template Name: Homepage
 			<!-- call to action b'doodle -->
 			<div id="home-cta">
 				<?php if (get_theme_mod('home_CTA_text') != '') : ?>
-					<h1><?php echo get_theme_mod('home_CTA_text');?></h1>
+					<h1><?php _e(get_theme_mod('home_CTA_text'));?></h1>
 				<?php endif; ?>
 
 				<?php if (!empty(get_theme_mod('home_CTA_link'))): ?>
 					<a href="<?php echo esc_url(get_permalink(get_theme_mod('home_CTA_link'))); ?>" class="button">
 						<?php if (!empty(get_theme_mod('home_CTA_link_text'))) {
-							echo get_theme_mod('home_CTA_link_text');
+							_e(get_theme_mod('home_CTA_link_text'));
 						} else {
-							echo get_the_title(get_theme_mod('home_CTA_link'));
+							_e(get_the_title(get_theme_mod('home_CTA_link')));
 						}?>
 					</a>
 				<?php endif; ?>
